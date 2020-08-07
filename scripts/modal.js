88f352e28faf1c57f2fff3929cd83ab97e4ea4e8
petros <pit.trak@gmail.com>
2020-08-07 15:52:38 +0300

    added new photos

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,63 +2,60 @@ $(document).ready(function() {
   // MODAL
   var modalText = {
     discover: {
-      title: 'ChowNow Discover',
-      tag: 'FOOD ORDERING PLATFORM.',
+      title: 'React project',
+      tag: 'USING REOUTER REDUX AND HOOKS.',
       detail:
-        'ChowNow Discover is a platform that lets customers discover new local restaurants and provides business owners with tools to convert first time orders into lifelong diners.',
-      link: 'https://eat.chownow.com/'
+        'A single-page web-application using React framework with Router, Redux and Hooks. In this project I implemented google authentication for logging into the application and set up firebase for storing the data in online database. Aside from HTML and JSX, SCSS is used for implementing the styles. The development of this app has been organised with webpack.',
+      link: 'https://react-project-no1.herokuapp.com/'
     },
     ordering: {
-      title: 'ChowNow Ordering Web',
-      tag: 'WHITE-LABEL ONLINE ORDERING SOLUTION.',
+      title: 'An introduction to React',
+      tag: 'FIRST INTRODUCTION TO REACT-JS.',
       detail:
-        'ChowNow is a commission-free online ordering system and food ordering app helping restaurants feed their hungry customers.',
-      link: 'https://direct.chownow.com/direct/195/locations/260'
+        'This was my first project using React framework. It is a simple single-page application where you can type into an array a pastime and do some basic functions such as edit and/ or delete elements of the array. At the end, you can randomly pick an element of the array to appear on a pop-up window. Aside from HTML and JSX, SCSS is used for implementing the styles. The development of this app has been organised with webpack.',
+      link: 'https://react-project-no0.herokuapp.com/'
     },
     newrelic: {
-      title: 'NewRelic.com',
-      tag: 'APPLICATION PERFORMANCE MONITORING.',
+      title: 'Spring Boot Application',
+      tag: 'SUPPORTS TOKER BASED AUTHENTICATION.',
       detail:
-        'Roambi provides analytics, reporting, and business intelligence for companies to use on the go. A Wordpress hosted site written in PHP and Javascript with Hubspot Integration.',
-      link: 'http://www.newrelic.com'
+        'A Spring Boot application architecture with Spring Security that works with JSON web tokens. Spring Data JPA interact also with MySQL database. The front-end is structured with React-js framework, LocalStorage, React Router and Axios. The project (client-server-database) is bundled and running with Docker compose.',
+      link: 'https://github.com/petrostrak/Spring-Boot-Token-based-Authentication-with-Spring-Security-JWT'
     },
     roambi: {
-      title: 'Roambi.com',
-      tag: 'BUSINESS ANALYTICS.',
+      title: 'AirSense',
+      tag: 'SPRINGBOOT APPLICATION.',
       detail:
-        'Roambi provides analytics, reporting, and business intelligence for companies to use on the go. A Wordpress hosted site written in PHP and Javascript with Hubspot Integration.',
-      link: 'http://www.roambi.com'
+        'AirSense is an air quality IoT sensors e-shop and data visualization service and my first collaborate work with Agile (SCRUM) methodology. It is structured with REST API architecture. The back-end is set up with Spring Boot, JPA and Hibernate while the front-end with React-js, Bootstrap and CSS. The application implements chat using Websockets, Paypal transactions and SMTP Mail service. Last but not least, a python script generates dummy data and store them to MySQL and Grafana analyse and visualize them on the application.',
+      link: 'https://github.com/petrostrak/airSense'
     },
     walker: {
-      title: 'WalkerTracker',
-      tag: 'PERFORMANCE METRICS.',
+      title: 'Silver Spoon Application',
+      tag: 'ANGULAR-JS APPLICATION.',
       detail:
-        'Walker Tracker offers goal management, fitness tracking, and team competitions to companies for internal use. A Ruby on Rails and Javascript companion site for the Walker Tracker App. Features visual metrics and gamified progression system.'
-    },
+        'An AngularJs web-application where the user can store food recipes in firebase. For this application I used NgRoute for the communication of the components, NgForms, NgHttp for the requests to the database, Subjects as well as Lazy Loading in order to minimize the load of the app.',
+      link: 'https://silver-spoon-2e1cf.web.app/auth',
+      },
     powur: {
-      title: 'Powur.com',
-      tag: 'CONSUMER POWERED MARKETING.',
+      title: 'Chat Application',
+      tag: 'NODE-JS APPLICATION.',
       detail:
-        'Powur is a marketing platform for lead generation, recruitment, and team building. Built with Ruby on Rails and Angular-UI. Makes use of Angular-material for front-end visuals. Features complex user tree heiarchy and commission system.',
-      link: 'http://www.powur.com/with/42'
+        'A node.js chat-application. It gives the user the freedom to choose an individual private room to join and chat with others. The back-end is written in Node.js taking advantage of the socket.io library and the front-end is rendered by a simplistic HTML / CSS.',
+      link: 'https://petros-chat-app.herokuapp.com/'
     },
     mystand: {
-      title: 'MyStand',
-      tag: 'CROWD-FUNDED CHARITY.',
+      title: 'Weather Application',
+      tag: 'NODE-JS APPLICATION.',
       detail:
-        'MyStand is a crowd-funding, media sharing website, that has you donating actions instead of money out of your pocket. Single page App built with Node.js on Sails and Angular 2.0. Features social media sharing and large scale crowd-funding.'
+        'A node.js web-application where the user can search the weather forecast. It makes http calls to Open Weather Map API and renders the result on the screen. The back-end is written in Node.js and the front-end is rendered by a simplistic HTML / CSS.',
+      link: 'https://petros-weather-app.herokuapp.com/'
     },
     never: {
-      title: 'NeverSurrender',
-      tag: 'ALS AWARENESS.',
+      title: 'Task Manager Application',
+      tag: 'NODE-JS APPLICATION.',
       detail:
-        'NeverSurrender is a platform for the new ALS foundation mobile app in hopes to raise awareness and research funding to fight ALS. Pure JavaScript marketing site to promote the new ALS NeverSurrender app.'
-    },
-    themall: {
-      title: 'The Mall',
-      tag: 'PEER GUIDED SHOPPING.',
-      detail:
-        'The Mall is a place to follow the latest fashion purchases of your friends and favorite celebrities. Built with Node.js and Handlebars. Features the ability to import thousands of top brands products into one shopping site.'
+        'A node.js Task Manager Rest API. It uses user authentication with email / password, supports Email notification and database datastorage file-upload in MongoDB.',
+      link: 'https://github.com/petrostrak/Task-Manager-App',
     }
   };
 
