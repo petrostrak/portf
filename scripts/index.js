8029607491fb292bd9ee72c3c0c41224f64354ff
petros <pit.trak@gmail.com>
2020-09-29 11:35:12 +0300

    Updated skill numbers

--- scripts/index.js
+++ scripts/index.js
@@ -179,23 +179,6 @@ $(function() {
     onScrollInit($('.waypoint'));
   }, 10);
 
-  // CONTACT FORM
-  $('#contact-form').submit(function(e) {
-    e.preventDefault();
-
-    $.ajax({
-      url: 'https://formspree.io/mattwilliams85@gmail.com',
-      method: 'POST',
-      data: { message: $('form').serialize() },
-      dataType: 'json'
-    }).done(function(response) {
-      $('#success').addClass('expand');
-      $('#contact-form')
-        .find('input[type=text], input[type=email], textarea')
-        .val('');
-    });
-  });
-
   $('#close').click(function() {
     $('#success').removeClass('expand');
   });
